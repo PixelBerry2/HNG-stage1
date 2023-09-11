05cc1dfa28e0e2e5fd85ac12fcb75ce622a5f7b4
PixelBerry2 <132242126+PixelBerry2@users.noreply.github.com>
2023-09-11 23:20:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let outputDate = document.querySelector('.date');
 let outputTime = document.querySelector('.current-time');
 
-    let today = new Date();
-    
-    let date = today.toLocaleDateString("en-US", { weekday: "long" });;
+let today = new Date();
 
-    let current_date = `${date}`;
-    outputDate.innerText = current_date;
+let date = today.toLocaleDateString("en-US", { weekday: "long" });
+let current_date = `${date}`;
+outputDate.innerText = current_date;
 
-    function updateCurrentTime() {
-        const currentTimeMilliseconds = new Date().getTime();
-        outputTime.innerText = currentTimeMilliseconds;
-      }
+function updateCurrentTime() {
+  const currentTimeMilliseconds = new Date().getTime();
+  outputTime.innerText = currentTimeMilliseconds;
+}
+
       setInterval(updateCurrentTime, 100);
